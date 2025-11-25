5ad295526cf32ddbf2804b21691e59015c3472c3
danil <kornachenkodanil@gmail.com>
2025-11-25 15:11:33 +0700

    feat(main) : Completed

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -11,13 +11,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lemonade.ui.theme.LemonadeTheme
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -30,7 +35,6 @@ class MainActivity : ComponentActivity() {
             LemonadeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -40,15 +44,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column(modifier = modifier,
+fun Greeting(modifier: Modifier = Modifier) {
+    var result by remember { mutableStateOf(1) }
+    val imageResource = when (result) {
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_squeeze
+        3 -> R.drawable.lemon_drink
+        else -> R.drawable.lemon_restart
+    }
+    val stringResource = when (result) {
+        1 -> R.string.Lemon_tree
+        2 -> R.string.Lemon
+        3 -> R.string.Glass_of_lemonade
+        else -> R.string.Empty_glass
+    }
+    Column(modifier = modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center),
         horizontalAlignment = Alignment.CenterHorizontally) {
         Image(
-            painter = painterResource(R.drawable.lemon_tree),
-            contentDescription = "lemon_tree"
+            painter = painterResource(imageResource),
+            contentDescription = result.toString(),
+            modifier = Modifier
+                .wrapContentSize()
+                .clickable {
+                    if (result < 4) {
+                        result = result + 1
+                    } else {
+                        result = 1
+                    }
+                }
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Text(stringResource(R.string.Lemon_tree))
+        Text(stringResource(stringResource))
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(stringResource(R.string.Dev))
+        /*Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            if (result < 4) {
+                result = result + 1
+            } else {
+                result = 1
+            }
+        }) { }*/
     }
 }
 
@@ -56,6 +94,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     LemonadeTheme {
-        Greeting("Android")
+        Greeting(modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center))
     }
 }
